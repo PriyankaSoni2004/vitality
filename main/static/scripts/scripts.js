a65d1561eb3a3da609046a0c24b181bf7ac9b8bb
Raghav Goel <goelraghav01@gmail.com>
2023-09-19 09:30:38 +0530

    frontend final

--- main/static/scripts/scripts.js
+++ main/static/scripts/scripts.js
@@ -1,26 +1,6 @@
 $(document).ready(function () {
 
-    // Default to Text Input
-    $("#div-textarea").show()
-    $("#div-url").hide()
-    $("#div-media").hide()
-
-    // Show and Hide DIVs based on button click.
-    $("#btn-prompt-text").click(() => {
-        $("#div-textarea").show()
-        $("#div-url").hide()
-        $("#div-media").hide()
-    });
-    $("#btn-prompt-url").click(() => {
-        $("#div-textarea").hide()
-        $("#div-url").show()
-        $("#div-media").hide()
-    });
-    $("#btn-prompt-media").click(() => {
-        $("#div-textarea").hide()
-        $("#div-url").hide()
-        $("#div-media").show()
-    });
+    $("#div-media").show()
 
     // Form Submit and Show Overlay.
     $("#btn-find-sentiment").click((e) => {
@@ -28,16 +8,10 @@ $(document).ready(function () {
 
         let formData = new FormData();
 
-        if ($("#div-textarea").is(":visible")) {
-            formData.append("input", $("#form-input-textarea").val());
-            formData.append("type", "text")
-        } else if ($("#div-url").is(":visible")) {
-            formData.append("input", $("#form-input-url").val());
-            formData.append("type", "url")
-        } else if ($("#div-media").is(":visible")) {
-            formData.append("input", $("#form-input-media")[0].files[0]);
-            formData.append("type", "media")
-        }
+    if ($("#div-media").is(":visible")) {
+        formData.append("input", $("#form-input-media")[0].files[0]);
+        formData.append("type", "media")
+    }
 
         if (formData.get("input") != '') {
             $("#sentiment-overlay").css("display", "block");
@@ -50,10 +24,15 @@ $(document).ready(function () {
                 processData: false,
             })
                 .done((response) => {
-                    val_neg = Math.round(parseFloat(response["score_negative"]) * 10000) / 100
                     val_neu = Math.round(parseFloat(response["score_neutral"]) * 10000) / 100
-                    val_pos = Math.round(parseFloat(response["score_positive"]) * 10000) / 100
-
+                    val_cal = Math.round(parseFloat(response["score_calm"]) * 10000) / 100
+                    val_hap = Math.round(parseFloat(response["score_happy"]) * 10000) / 100
+                    val_sad = Math.round(parseFloat(response["score_sad"]) * 10000) / 100
+                    val_ang = Math.round(parseFloat(response["score_angry"]) * 10000) / 100
+                    val_fea = Math.round(parseFloat(response["score_fearful"]) * 10000) / 100
+                    val_dis = Math.round(parseFloat(response["score_disgust"]) * 10000) / 100
+                    val_sur = Math.round(parseFloat(response["score_surprised"]) * 10000) / 100
+                    
                     $("#sentiment-overlay-content").html(`
                         <h2>PROMINENT SENTIMENT</h2>
                         <p id="prominent-sentiment">` + response["prominent_sentiment"] + `</p>
@@ -67,19 +46,44 @@ $(document).ready(function () {
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>&#128543;</td>
-                                            <td>Negative</td>
-                                            <td>` + val_neg + `%</td>
-                                        </tr>
-                                        <tr>
                                             <td>&#128528;</td>
-                                            <td>Neutral</td>
+                                            <td>neutral</td>
                                             <td>` + val_neu + `%</td>
                                         </tr>
                                         <tr>
+                                            <td>&#128524;</td>
+                                            <td>calm</td>
+                                            <td>` + val_cal + `%</td>
+                                        </tr>
+                                        <tr>
                                             <td>&#128512;</td>
-                                            <td>Positive</td>
-                                            <td>` + val_pos + `%</td>
+                                            <td>happy</td>
+                                            <td>` + val_hap + `%</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&#128532;</td>
+                                            <td>sad</td>
+                                            <td>` + val_sad + `%</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&#128544;</td>
+                                            <td>angry</td>
+                                            <td>` + val_ang + `%</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&#128552;</td>
+                                            <td>fearful</td>
+                                            <td>` + val_fea + `%</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&#129314;</td>
+                                            <td>disgust</td>
+                                            <td>` + val_dis + `%</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&#128562;</td>
+                                            <td>surprised</td>
+                                            <td>` + val_sur + `%</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -92,10 +96,10 @@ $(document).ready(function () {
                     new Chart(chart, {
                         type: 'radar',
                         data: {
-                            labels: ['Negative', 'Neutral', 'Positive'],
+                            labels: ['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprised'],
                             datasets: [{
                                 label: 'Sentiment',
-                                data: [val_neg, val_neu, val_pos],
+                                data: [val_neu, val_cal, val_hap, val_sad,val_ang,val_fea,val_dis,val_sur],
                                 fill: true,
                                 color: '#FFFFFF',
                                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
